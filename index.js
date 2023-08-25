3852e5fd7cc6b3a3df583ff9b7919c921d188bdc
takam <takamalexchristian@gmail.com>
2023-08-25 21:36:07 +0800

    more structural implementations

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ expressApp.use(bodyParser.json());
 
 
 //db connection here
-mongoose.connect(process.env.DB_URI).catch((err)=>{
+mongoose.connect(process.env.DB_URI).catch((err) => {
     // console.log("connection mostlikely failed")
     throw new Error(err) // connection
 });
@@ -31,72 +31,88 @@ mongoose.connect(process.env.DB_URI).catch((err)=>{
 //schemas
 
 const UserSchema = new mongoose.Schema({
-    _id: mongoose.SchemaTypes.ObjectId,
     username: String,
     password: String, // will be replaced by a hash later
-    session: String,
-    collections: [{
-        _id: [mongoose.SchemaTypes.ObjectId],
-    }]
+    _authToken: mongoose.SchemaTypes.ObjectId, //the 
+    collectionsIdArray: [mongoose.SchemaTypes.ObjectId]
 })
 
 const CollectionSchema = new mongoose.Schema({
-    _id: mongoose.SchemaTypes.ObjectId,
-    _ownerId: mongoose.SchemaTypes.ObjectId,
+    _ownerUserId: mongoose.SchemaTypes.ObjectId,
     name: String,
     lastConsulted: {
         type: Date,
-        default: ()=>{
+        default: () => {
             return Date.now();
         }
     },
 })
 
 const NoteSchema = new mongoose.Schema({
-    _id: mongoose.SchemaTypes.ObjectId,
-    _collectionId: mongoose.SchemaTypes.ObjectId,// one note is not expected to be owned by multiple collections
+    _ownerCollectionId: mongoose.SchemaTypes.ObjectId,// one note is not expected to be owned by multiple collections
     title: String,
     body: String,
     tags: [String],
     creationDate: {
         type: Date,
-        default: ()=>{
+        default: () => {
             return Date.now()
         }
     },
     lastModified: {
         type: Date,
-        default: ()=>{
+        default: () => {
             return Date.now()
         }
     }
 })
 
 //models 
-const UsersModel = mongoose.model("User", UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 const CollectionModel = mongoose.model("Collection", CollectionSchema);
 const NoteModel = mongoose.model("Note", NoteSchema);
 
 //routes handlers
 
-expressApp.route("/users")
+expressApp.route("/user")
     .get((req, res) => {
-        res.send("users getted");
+        UserModel.findOne({ username: req.body.username, password: req.body.password }).then(doc => {
+            console.log(doc);
+            res.send({ message: "welcome back " + doc.username });
+        })
     })
-    .post((req, res)=>{
-        res.send("new user posted");
+    .post((req, res) => {
+        //we validate the data received
+        let newUserModel = new UserModel({
+            username: req.body.username,
+            password: req.body.password,
+        })
+
+        newUserModel.save().then(newDoc => {
+            console.log(newDoc)
+
+            res.send(newDoc)
+        })
     })
-    .delete((req, res)=>{
+    .delete((req, res) => {
+        //user may terminate their account
         res.send("user deleted");
     })
-    .patch((req, res)=>{
+    .patch((req, res) => {
+        //user may change their password later
+        //user may change their username later
         res.send("user partially updated")
     })
-    .put((req, res)=>{
-        res.send("user resources entirely replaced")
+
+
+expressApp.route("/collection")
+    .get((req, res)=>{ //logic to get notes
+        res.send({message: "you get a list of user collections by id and name"})
+    })
+    .post((req, res)=>{
+        res.send({message: "new collection added"})
     })
 
-    
 expressApp.listen(port, () => {
     console.log(`server running on port ${port}`)
 })
